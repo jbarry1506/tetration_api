a8815b208f5285bdd159807de6c6d74c38d1a88d
jbarry1506 <jbarry1506@gmail.com>
2019-09-05 15:29:00 -0400

    Added DELETE Application

--- applications.py
+++ applications.py
@@ -24,7 +24,29 @@ resp = rc.get(CURRENT_GET_ENDPOINT)
 # make sure we get a good response from the request
 if resp.status_code != 200:
    print("Unsuccessful request returned code: {} , \
-       response: {}".format(resp.status_code,resp.text))
+       response: {}".format(resp.status_code,resp.reason))
 results = resp.json()
 
 pprint.pprint(results[0])
+
+#---------------------------------------------------------------------------------------
+
+# Delete Application Workspace
+
+# Create the Tetration REST Client 
+rc = RestClient(API_ENDPOINT, api_secret=API_SECRET, api_key=API_KEY, verify=False)
+
+# this is already deleted
+deleteme = '5d71196e755f0254fbc8615b'
+
+DEL_ENDPOINT = "/openapi/v1/applications/"+deleteme
+print(DEL_ENDPOINT)
+resp = rc.delete(DEL_ENDPOINT)
+
+# make sure we get a good response from the request
+if resp.status_code != 200:
+   print("Unsuccessful request returned code: {} , \
+       response: {}".format(resp.status_code,resp.reason))
+results = resp.json()
+
+pprint.pprint(results.success)
